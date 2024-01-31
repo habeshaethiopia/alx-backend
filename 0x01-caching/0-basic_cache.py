2dd32f4cf9c6274133f85a8b3336787a34c80d25
Adane <adanemoges6@gmail.com>
2024-01-31 15:20:06 +0300

    mine

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,4 +13,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Get an item by key"""
-        return self.cache_data[key] if key in self.cache_data else None
+        return self.cache_data.get(key, None)
