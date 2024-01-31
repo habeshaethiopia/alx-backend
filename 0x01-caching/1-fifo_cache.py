80865c2a5ee6365bb3220dc2cea382d380a570bf
Adane <adanemoges6@gmail.com>
2024-01-31 15:24:58 +0300

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,13 +5,18 @@ from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
     """fifi cache"""
+    def __init__(self):
+        """constructor"""
+        super().__init__()
+        self.queue = []
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key and item:
             self.cache_data[key] = item
+            self.queue.append(key)
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first = self.cache_data.keys()[0]
+            first = self.queue[0]
             self.cache_data.pop(first)
             print("DISCARD: {}".format(first))
 
