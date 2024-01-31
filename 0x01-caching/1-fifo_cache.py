152efeda1afe2296a0dbb56e9c799a514037727d
Adane <adanemoges6@gmail.com>
2024-01-31 15:28:43 +0300

    tsk 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,25 +1,48 @@
-#!/usr/bin/env python3
-"""class FIFOCache that inherits from BaseCaching"""
+#!/usr/bin/python3
+""" FIFO Caching """
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """fifi cache"""
+    """FIFO caching"""
+
     def __init__(self):
-        """constructor"""
+        """Constructor"""
         super().__init__()
         self.queue = []
 
     def put(self, key, item):
-        """Add an item in the cache"""
-        if key and item:
-            self.cache_data[key] = item
+        """Puts item in cache"""
+        if key is None or item is None:
+            return
+
+        if key not in self.queue:
             self.queue.append(key)
+        else:
+            self.mv_last_list(key)
+
+        self.cache_data[key] = item
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first = self.queue[0]
-            self.cache_data.pop(first)
-            print("DISCARD: {}".format(first))
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
 
     def get(self, key):
-        """Get an item by key"""
-        return self.cache_data[key] if key in self.cache_data else None
+        """Gets item from cache"""
+        return self.cache_data.get(key, None)
+
+    def mv_last_list(self, item):
+        """Moves element to last idx of list"""
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """Get first element of list or None"""
+        return array[0] if array else None
